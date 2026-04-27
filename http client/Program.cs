a6333ec532804ab1f73132dbe426b8b505725342
hyunuk <shu.emp@gmail.com>
2026-04-27 15:25:52 +0900

    Update Program.cs

--- http client/Program.cs
+++ http client/Program.cs
@@ -20,7 +20,15 @@ namespace ConsoleApp2
         public List<string> inputQueueURIs { get; set; }
         public string outputQueueURI { get; set; }
     }
-
+    // @@@@@ 1
+    public class InputData
+    {
+        public long timestamp { get; set; }
+        public string value { get; set; }
+        // custom 추가
+        public string queueNumber { get; set; }
+    }
+    
     internal class Program
     {
         // ##### 2
@@ -70,16 +78,90 @@ namespace ConsoleApp2
                 return null;
             }    
          }
+        // @@@@@ 2
+         public static InputData RequestQueueInfo_Easy(string target, string queueNumber) // easy  // api 에서 제공하는 result check 사용 // 코드 짧음.
+         {
+            string url = target;
+            Console.WriteLine(url);
+            if (url == null)
+            {
+                return null;
+            }
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    var res = client.GetAsync(url).Result;
+                    if (!res.IsSuccessStatusCode)
+                    {
+                        Console.WriteLine($"HTTP Error: {res.StatusCode}");
+                        return null;
+                    }
+                    string json = res.Content.ReadAsStringAsync().Result;
+                    InputData result = JsonConvert.DeserializeObject<InputData>(json); // 받은 값 처리위해 return.
+                    result.queueNumber = queueNumber; // pass number 추가 (각 thread에서 처리 위해 추가 정보.)
+                    Console.WriteLine($"received Q:{queueNumber} timestamp={result.timestamp}, value={result.value}");
+                    return result;
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("Request error: " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected error: " + e.Message);
+            }
+            return null;        
+        }
 
+        // @@@@@ 5
+        public static void PassResult(string targetUri, string resultStr)
+        {
+            string url = targetUri;
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    // JSON 객체 생성
+                    var bodyObj = new
+                    {
+                        result = resultStr // lavel and value
+                    };
+                    // JSON 문자열 변환
+                    string json = JsonConvert.SerializeObject(bodyObj);
+                    Console.WriteLine("post req: {0} {1} {2}", json, resultStr, bodyObj);
+                    var content = new StringContent(json, Encoding.UTF8, "application/json");
+                    var res = client.PostAsync(targetUri, content).Result;
+                    if (!res.IsSuccessStatusCode)
+                    {
+                        Console.WriteLine($"HTTP Error: {res.StatusCode}");
+                        return;
+                    }
+                    string response = res.Content.ReadAsStringAsync().Result;
+                    Console.WriteLine("Output Response: {0} " + response);
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("Request error: " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected error: " + e.Message);
+            }
+            return;        
+        }
+        
         static void Main(string[] args) // http
         {
 
-            /*
+            /*  // HTTP GET sample. simple.
             HttpClient client = new HttpClient();            
             var res = client.GetAsync("http://127.0.0.1:8080/helloworld").Result;
             Console.WriteLine("Response : " + res.StatusCode + " - " + res.Content.ReadAsStringAsync().Result);
             */
-            /*
+            /*  // HTTP POST sample. simple.
             HttpClient client = new HttpClient();
             var json_obj = new JObject();
             json_obj.Add("data1", "some data");
@@ -91,7 +173,36 @@ namespace ConsoleApp2
             // ##### 3
             QueueInfo queueInfo = GetQueueInfo(); // Http client GET -> response check -> json to struct deserialize
 
-
+            // @@@@@ 3
+            for (int i = 0; i < queueInfo.inputQueueURIs.Count; i++)
+            {
+                int queueNo = i;
+                string uri = queueInfo.inputQueueURIs[i];
+            
+                Task.Run(() => // 각 http server들을 thread 로 분리하여 실행. (비동기 필수)
+                {
+                    Console.WriteLine("worker run ", queueNo);
+                    while (true)
+                    {
+                        Console.WriteLine(".");
+                        //InputData result = RequestQueueInfo_Easy(uri, i.ToString());//Task.Run 안에서 i.ToString() 쓰면 위험
+                        InputData result = RequestQueueInfo_Easy(uri, queueNo.ToString());
+                        if (result == null)
+                        {
+                            Console.WriteLine($"Q:{queueNo} result is null");
+                            continue;
+                        }
+                        // @@@@@ 4
+                        string workerResult = procWorker(result);
+                        if (workerResult != null) // return값 validation 체크를 안해서 big issue.
+                        {
+                            Console.WriteLine("procWorker RES: {0} {1} ", workerResult, queueInfo.outputQueueURI);
+                            // @@@@@ 5
+                            PassResult(queueInfo.outputQueueURI, workerResult);
+                        }
+                    }
+                });
+            }
             
 
             // 폴더내 파일 목록을 JSON으로 변환하여 서버로 POST 하는 샘플
